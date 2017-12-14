a388bc89d8975872790308f3dcf234c7f7d3cdb7
Animesh <Animesh@DESKTOP-K6SBNEE>
2017-12-13 18:45:59 -0700

    Added the reliefF code to main.py

--- DDoS_Detection/src/main.py
+++ DDoS_Detection/src/main.py
@@ -2,11 +2,13 @@
 Created on 11-Oct-2017
 @author: animesh
 '''
+from __future__ import print_function
 from numpy import record
-import numpy
+import numpy as np
 import math
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import SelectKBest
+from sklearn.neighbors import KDTree
 import pandas
 import csv,sys
 
@@ -126,10 +128,122 @@ def chi2_list():
     chi2_dict = dict(zip(feature, chi2_list))
     print("The chi-squared values are: ")
     # summarize scores
-    numpy.set_printoptions(precision=3)
+    np.set_printoptions(precision=3)
     print(chi2_dict)
     print("The sorted chi-squared values are: ")
     print(sorted(chi2_dict, key = chi2_dict.get))
+
+class reliefF(object):
+    """Feature selection using data-mined expert knowledge.
+
+    Based on the ReliefF algorithm as introduced in:
+
+    Kononenko, Igor et al. Overcoming the myopia of inductive learning algorithms with RELIEFF (1997), Applied Intelligence, 7(1), p39-55
+
+    """
+
+    def __init__(self, n_neighbors=10, n_features_to_keep=10):
+        """Sets up ReliefF to perform feature selection.
+
+        Parameters
+        ----------
+        n_neighbors: int (default: 100)
+            The number of neighbors to consider when assigning feature importance scores.
+            More neighbors results in more accurate scores, but takes longer.
+
+        Returns
+        -------
+        None
+
+        """
+        
+        self.feature_scores = None
+        self.top_features = None
+        self.tree = None
+        self.n_neighbors = n_neighbors
+        self.n_features_to_keep = n_features_to_keep
+
+    
+    def fit(self, X, y):
+        """Computes the feature importance scores from the training data.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Training instances to compute the feature importance scores from
+        y: array-like {n_samples}
+            Training labels
+
+        Returns
+        -------
+        None
+
+        """
+        self.feature_scores = np.zeros(41)
+        self.tree = KDTree(X)
+        #The shape attribute for numpy arrays returns the dimensions of the array. 
+        #If Y has n rows and m columns, then Y.shape is (n,m). So Y.shape[0] is n
+        for source_index in range(X.shape[0]):
+            distances, indices = self.tree.query(X[source_index].reshape(1, -1), k=self.n_neighbors + 1)
+            #Distances returns the distances of top 10 nearest neighbors from the row in question
+            #Indices returns the location of those those 10 nearest neighbors
+          
+            # First match is self, so ignore it
+            for neighbor_index in indices[0][1:]:
+                similar_features = X[source_index] == X[neighbor_index]
+                label_match = y[source_index] == y[neighbor_index]
+                
+                # If the labels match, then increment features that match and decrement features that do not match
+                # Do the opposite if the labels do not match
+                if label_match:
+                    self.feature_scores[similar_features] += 1.
+                    self.feature_scores[~similar_features] -= 1.
+                else:
+                    self.feature_scores[~similar_features] += 1.
+                    self.feature_scores[similar_features] -= 1.
+        self.top_features = np.argsort(self.feature_scores)[::-1]
+        reliefF_list = self.feature_scores
+        reliefF_dict = dict(zip(feature, reliefF_list))
+        print("The reliefF values are:")
+        print(reliefF_dict)
+        print("The sorted reliefF values are:")
+        print(sorted(reliefF_dict, key = reliefF_dict.get))
+        #print(self.feature_scores)
+        #print(self.top_features)
+    
+    def transform(self, X):
+        """Reduces the feature set down to the top `n_features_to_keep` features.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Feature matrix to perform feature selection on
+
+        Returns
+        -------
+        X_reduced: array-like {n_samples, n_features_to_keep}
+            Reduced feature matrix
+
+        """
+        return X[:, self.top_features[:self.n_features_to_keep]]
+
+    def fit_transform(self, X, y):
+        """Computes the feature importance scores from the training data, then reduces the feature set down to the top `n_features_to_keep` features.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Training instances to compute the feature importance scores from
+        y: array-like {n_samples}
+            Training labels
+        Returns
+        -------
+        X_reduced: array-like {n_samples, n_features_to_keep}
+            Reduced feature matrix
+
+        """
+        self.fit(X, y)
+        return self.transform(X)
 
 def main():
     print("Starting application..")
@@ -137,6 +251,7 @@ def main():
     print("1 - Display the entropy list")
     print("2 - Display the information gain list")
     print("3 - Display the chi-squared list")
+    print("4 - Display the ReliefF list")
     selection = input("Enter your selection: ")
     if(selection == 1):
         ent_list(traffic)
@@ -144,6 +259,15 @@ def main():
         ig_list(traffic)
     elif(selection == 3):
         chi2_list()
+    elif(selection == 4):
+        dataframe = pandas.read_csv(filename_adj, names=feature)
+        array = dataframe.values
+        X = array[:,0:41]
+        Y = array[:,41]
+        obj = reliefF()
+        obj.fit_transform(X, Y)
+        print("#################")
+        
     else:
         print("Invalid selection")
     
