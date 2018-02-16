c980382167d921b70309bc337029ca99261d5615
animesh <animesh@AnimeshUbuntu>
2018-02-16 12:23:05 -0700

    App basic

--- DDoS_Detection/src/app.py
+++ DDoS_Detection/src/app.py
@@ -3,26 +3,46 @@ Created on Feb 12, 2018
 
 @author: animesh
 '''
-from Tkinter import *
 
-root = Tk()
-firstFrame = Frame(root)
-firstFrame.pack(side = TOP)
-secondFrame = Frame(root)
-secondFrame.pack()
-thirdFrame = Frame(root)
-thirdFrame.pack()
-fourthFrame = Frame(root)
-fourthFrame.pack(side = BOTTOM)
+import Tkinter as tk
+from main import *
+
+root = tk.Tk()
+root.resizable(width=False, height=False)
+
+frame1 = tk.Frame(width=300, height=576, bg="", colormap="new")
+frame1.pack()
+FSlabel = tk.Label(frame1, text="Feature Selection: ").pack(side = 'left')
+#Drop down menu for feature selection
+variable = tk.StringVar(root)
+variable.set("Information Gain") # default value
+w = tk.OptionMenu(frame1, variable, "Information Gain", "Chi Squared", "ReliefF")
+w.pack()
+FSRlabel = tk.Label(frame1, text="Recommended: ReliefF").pack(side = 'left')
+
+clxlabel = tk.Label(frame1, text="Classification: ").pack(side = 'left')
+#Drop down menu for feature selection
+variable = tk.StringVar(root)
+variable.set("Information Gain") # default value
+w = tk.OptionMenu(frame1, variable, "Naive Bayes", "SVM", "Decision Trees", "Random Forest")
+w.pack()
+clxRlabel = tk.Label(frame1, text="Recommended: Decision Tree").pack(side = 'left')
+
+def callback():
+    print "click!"
+    main()
+
+b = tk.Button(frame1, text="Detect", command=callback)
+b.pack()
 
 
-greenbutton = Button(firstFrame, text="green", fg="green")
-greenbutton.pack( side = LEFT )
 
-separator = Frame(height=2, bd=1, relief=SUNKEN)
-separator.pack(fill=X, padx=5, pady=5)
 
-bluebutton = Button(secondFrame, text="blue", fg="blue")
-bluebutton.pack( side = LEFT )
+
+
+
+
+frame2 = tk.Frame(width=300, height=576, bg="", colormap="new")
+frame2.pack()
 
 root.mainloop()
